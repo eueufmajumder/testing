36a660caec6bb909ede11b1b8ded5f5d8b76f21f
eueufmajumder <55054006+eueufmajumder@users.noreply.github.com>
2019-10-07 08:48:56 -0800

    first push

---
+++ src/testing/Base.java
@@ -0,0 +1,13 @@
+package testing;
+
+import org.testng.annotations.Test;
+
+public class Base {
+	
+@Test
+public void test(){
+String a=("she is very beatufulgirls");
+int b=20;
+System.out.println(a+b);
+}
+}
